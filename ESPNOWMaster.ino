98a99fae88e7a8d5b16a9316d88d98c5d21baca1
Ambinintsoa Marie Aimée Vololonjo <83376122+Ambinintsoa4@users.noreply.github.com>
2022-12-16 18:04:56 +0100

    Version initiale ESP Emetteur

---
+++ ESPNOWMaster.ino
@@ -0,0 +1,128 @@
+#include <esp_now.h>
+#include <WiFi.h>
+
+#include <ESP32SharpIR.h>
+
+ESP32SharpIR sensor( ESP32SharpIR::GP2Y0A21YK0F, 32); //34
+
+#define led1 2
+#define led2 15
+
+// REPLACE WITH THE MAC Address of your receiver 94:B9:7E:D9:F1:54
+uint8_t broadcastAddress[] = {0x94, 0xB9, 0x7E, 0xD9, 0xF1, 0x54};
+
+// Define variables to store BME280 readings to be sent
+float distance;
+
+
+// Define variables to store incoming readings
+float incomingDatas;
+
+// Variable to store if sending data was successful
+String success;
+
+//Structure example to send data
+//Must match the receiver structure
+typedef struct struct_message {
+    float dist;
+    float data;
+} struct_message;
+
+// Create a struct_message called BME280Readings to hold sensor readings
+struct_message SHarpIRReadings;
+
+// Create a struct_message to hold incoming sensor readings
+struct_message incomingReadings;
+
+esp_now_peer_info_t peerInfo;
+
+// Callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+  if (status ==0){
+    success = "Delivery Success :)";
+  }
+  else{
+    success = "Delivery Fail :(";
+  }
+}
+
+// Callback when data is received
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memcpy(&incomingReadings, incomingData, sizeof(incomingReadings));
+  Serial.print("Bytes received: ");
+  Serial.println(len);
+  incomingDatas = incomingReadings.data;
+}
+ 
+void setup() {
+  // Init Serial Monitor
+  Serial.begin(115200);
+ 
+  pinMode(led1,OUTPUT);
+  pinMode(led2,OUTPUT);
+
+  sensor.setFilterRate(0.1f); 
+
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(OnDataSent);
+  
+  // Register peer
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+  
+  // Add peer        
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+  // Register for a callback function that will be called when data is received
+  esp_now_register_recv_cb(OnDataRecv);
+}
+ 
+void loop() {
+  getReadings();
+ 
+  // Set values to send
+  SHarpIRReadings.dist = distance;
+
+  // Send message via ESP-NOW
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &SHarpIRReadings, sizeof(SHarpIRReadings));
+   
+  if (result == ESP_OK) {
+    Serial.println("Sent with success");
+  }
+  else {
+    Serial.println("Error sending the data");
+  }
+ 
+    if (distance < 12.0f ) {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, LOW);
+    } 
+
+    if (distance > 13.0f ) {
+    digitalWrite(led2, HIGH); 
+    digitalWrite(led1, LOW);
+      }
+
+  Serial.println(distance);
+}
+
+void getReadings(){
+distance = sensor.getDistanceFloat();
+//distance = sensor.getRawDistance();
+}
+
